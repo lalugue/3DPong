c9488077bdf56af1156516ed69c5050f87c2fd57
lalugue <51893731+lalugue@users.noreply.github.com>
2020-05-30 20:29:27 +0800

    move opponent with velocity

--- Assets/OpponentMovement.cs
+++ Assets/OpponentMovement.cs
@@ -7,25 +7,44 @@ public class OpponentMovement : MonoBehaviour
     public int velocity = 5;
     public GameObject ball;
     Rigidbody rb;
+    double lastTime = 0;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        Debug.Log("The fixed time interval is: " + Time.fixedDeltaTime);
+     
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(ball != null) {
             Vector3 oldposition = this.transform.position;
             Vector3 target = new Vector3(ball.transform.position.x,0,0);
-            target = new Vector3(target.x - oldposition.x, 0, 0);
+            target = new Vector3(target.x,oldposition.y,oldposition.z);
+            //target = new Vector3(target.x - oldposition.x, 0, 0);
+           
             
             
-            //this.transform.position += (target - oldposition) * velocity * Time.deltaTime;
-            rb.MovePosition(oldposition + (target * velocity * Time.deltaTime));
+            //this.transform.position += (target - oldposition) * velocity * Time.deltaTime;            
+            //rb.MovePosition(target * velocity * Time.deltaTime);
+            
+            rb.velocity = (target - oldposition).normalized * velocity;
+            lastTime = Time.time;
+            
             //Debug.Log("Target position is: " + this.transform.position);
+        }
+        
+    }
+
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name != "ball"){
+            this.transform.position = this.transform.position;
         }
         
     }
