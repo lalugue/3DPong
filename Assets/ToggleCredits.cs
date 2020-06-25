32933a76fe3bae713cbf7b3b983cef5623b5d6a4
lalugue <51893731+lalugue@users.noreply.github.com>
2020-06-26 00:02:55 +0800

    add credits

---
+++ Assets/ToggleCredits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCredits : MonoBehaviour
+{
+    public GameObject CurrentUI;
+    public GameObject NextUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    public void ToggleCreditsUI(){
+        HideCurrentUI();
+        ShowNextUI();
+    }
+
+    void HideCurrentUI(){       
+       CurrentUI.SetActive(false);
+    }
+
+    void ShowNextUI(){
+        NextUI.SetActive(true);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
